facada10d6a93e9438a60e9577e4f60890cc813d
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-10-06 22:48:49 -0500

    landing page

--- dist/museum_of_code-4ac6972507c60ad4.js
+++ dist/museum_of_code-4ac6972507c60ad4.js
@@ -464,7 +464,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('museum_of_code-2ff738bec75fa6c0_bg.wasm', import.meta.url);
+        input = new URL('museum_of_code-4ac6972507c60ad4_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
