281f3c1abf3391d90ddf20bd10aa97ab6af13d43
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-10-06 23:24:09 -0500

    example haskell qs on landing page

--- dist/museum_of_code-8bdf2ad4b78c20f0.js
+++ dist/museum_of_code-8bdf2ad4b78c20f0.js
@@ -464,7 +464,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('museum_of_code-4ac6972507c60ad4_bg.wasm', import.meta.url);
+        input = new URL('museum_of_code-8bdf2ad4b78c20f0_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
