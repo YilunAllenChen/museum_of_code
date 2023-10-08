1f970d908c01aca5a8849777d1b9c3cb2c037ccd
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-10-08 10:00:52 -0500

    responsive home and wip page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -44,22 +44,22 @@ qs (x:xs) = qs bot ++ [x] ++ qs top
                         <div class="text-center">
                             <h1 class="text-4xl font-bold tracking-wider text-gray-100 sm:text-6xl font-mono">{"Museum of Code"}</h1>
                             <p class="mt-6 text-lg leading-8 text-gray-300">{"A curated exhibit of exquisite programming artifacts."}</p>
-                            <div class="mt-10 flex items-center justify-center gap-x-6 mb-20">
+                            <div class="mt-10 flex flex-wrap items-center justify-center gap-x-6 mb-20">
                                 <button
                                     onclick={ctx.props().on_clicked.reform(|_| "TakeATour".into())}
-                                    class="rounded-md disabled bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    class="rounded-md w-full md:w-1/4 bg-indigo-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     {"Take A Tour"}
                                 </button>
-                                <a href="#"
+                                <button
                                     onclick={ctx.props().on_clicked.reform(|_| "Contribute".into())}
-                                    class="rounded-md disabled bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    class="rounded-md w-full md:w-1/4 bg-green-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     {"Contribute"}
-                                </a>
-                                <a href="#"
+                                </button>
+                                <button
                                     onclick={ctx.props().on_clicked.reform(|_| "About".into())}
-                                    class="rounded-md disabled bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    class="rounded-md w-full md:w-1/4 bg-yellow-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     {"About the Museum"}
-                                </a>
+                                </button>
                             </div>
                         </div>
                         <div class="bg-gray-800 lg:pl-20 text-gray-300 p-4  rounded-md justify-left items-left">
--- src/pages/wip.rs
+++ src/pages/wip.rs
@@ -29,8 +29,8 @@ impl Component for Wip {
                 <div class="relative isolate px-6 pt-14 lg:px-8">
                     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 bg-opacity-50">
                         <div class="text-center">
-                            <h1 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-4xl">{"🚧Under Construction🚧"}</h1>
-                            <p class="mt-6 text-lg leading-8 text-gray-300">{"This Page is Still Under Construction. Check back later!"}</p>
+                            <h1 class="text:lg md:text-4xl font-bold tracking-tight text-gray-100 sm:text-4xl">{"🚧Under Construction🚧"}</h1>
+                            <p class="mt-6 text-sm md:text-lg leading-8 text-gray-300">{"This Page is Still Under Construction. Check back later!"}</p>
                             <div class="mt-10 flex items-center justify-center gap-x-6 mb-20">
                                 <button
                                     onclick={ctx.props().on_clicked.reform(|_| "Home".into())}
