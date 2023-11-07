164a161623456273077d5714c164d145bd7db1fe
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-11-06 21:48:41 -0600

    better home page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -31,6 +31,23 @@ impl Component for Home {
         let navigator = ctx.link().navigator().unwrap();
         let nav = |page| Callback::from(move |_| navigator.push(&page));
 
+        let buttons = vec![
+            ("Enter the Museum", Route::ExhibitionHall, "indigo"),
+            ("About the Museum", Route::About, "yellow"),
+            ("Help the Museum", Route::Contact, "green"),
+        ]
+        .into_iter()
+        .map(|(text, route, color)| {
+            html! {
+                <button
+                    onclick={nav.clone()(route)}
+                    class={format!("rounded-md w-full md:w-fit bg-{}-600 my-2.5 px-5 py-3 text-sm font-semibold text-white hover:bg-{}-800", color, color)}
+                >
+                    {text}
+                </button>
+            }
+        });
+
         html! {
             <div class="ease-in bg-black h-full">
                 <div class="mx-6 sm:mx-10 md:mx-16 lg:mx-36 mt-24 lg:mt-40">
@@ -38,25 +55,11 @@ impl Component for Home {
                         <h1 class="text-4xl font-bold tracking-wider text-gray-100 sm:text-6xl font-mono">{"Museum of Code"}</h1>
                         <p class="mt-6 text-lg leading-8 text-gray-300">{"A curated exhibit of exquisite programming artifacts."}</p>
                         <div class="mt-10 flex flex-wrap items-center justify-center gap-x-6 mb-20">
-                            <button
-                                onclick={nav.clone()(Route::ExhibitionHall)}
-                                class="rounded-md w-full md:w-1/4 bg-indigo-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                {"Enter the Museum"}
-                            </button>
-                            <button
-                                onclick={nav.clone()(Route::About)}
-                                class="rounded-md w-full md:w-1/4 bg-yellow-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                {"About the Museum"}
-                            </button>
-                            <button
-                                onclick={nav.clone()(Route::Contact)}
-                                class="rounded-md w-full md:w-1/4 bg-green-600 my-2.5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                {"Help the Museum"}
-                            </button>
+                            {for buttons}
                         </div>
                     </div>
                     <div class="bg-gray-800 md:p-12 mx-auto max-w-xl text-gray-300 p-4 md:py-8 mb-8 rounded-md justify-left items-left">
-                        <div class="mx-auto text-sm md:text-lg font-mono text-center w-3/4 py-2 bg-gray-700 ring ring-8 ring-gray-800 rounded relative -top-8 md:-top-12 text-gray-300">{"⭐ Featured Artifact ⭐"}</div>
+                        <div class="mx-auto text-xs md:text-base font-mono text-center w-3/4 py-2 bg-gray-700 ring ring-8 ring-gray-800 rounded relative -top-8 md:-top-12 text-gray-300">{"  Season Spotlight  "}</div>
                         <pre>
                             {formatted_haskell}
                         </pre>
